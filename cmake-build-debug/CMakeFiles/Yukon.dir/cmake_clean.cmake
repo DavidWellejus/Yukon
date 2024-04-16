339216dddad60a1fcbf0589e398ba2385b374a18
Saleh Al-kabani <s110779@dtu.dk>
2024-04-16 10:58:23 +0200

    redigeret makecfilen

---
+++ cmake-build-debug/CMakeFiles/Yukon.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Yukon.dir/deck.c.obj"
+  "CMakeFiles/Yukon.dir/deck.c.obj.d"
+  "CMakeFiles/Yukon.dir/file.c.obj"
+  "CMakeFiles/Yukon.dir/file.c.obj.d"
+  "CMakeFiles/Yukon.dir/main.c.obj"
+  "CMakeFiles/Yukon.dir/main.c.obj.d"
+  "CMakeFiles/Yukon.dir/shuffle.c.obj"
+  "CMakeFiles/Yukon.dir/shuffle.c.obj.d"
+  "CMakeFiles/Yukon.dir/table.c.obj"
+  "CMakeFiles/Yukon.dir/table.c.obj.d"
+  "Yukon.exe"
+  "Yukon.exe.manifest"
+  "Yukon.pdb"
+  "libYukon.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Yukon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
