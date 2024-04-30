74993b90c063f1510424a6ee1807b9960ab54245
salehvidovre <144039246+salehvidovre@users.noreply.github.com>
2024-04-30 22:07:57 +0200

    oprettet og tilføjet headerfil til main for at rydde op

--- main.c
+++ main.c
@@ -7,23 +7,7 @@
 #include "file.h"
 #include "shuffle.h"
 #include "table.h"
-
-
-void initializeDeck(Deck *deck);
-bool saveDeckToFile(const Deck *deck, const char *filename);
-Deck* shuffleDeck(Deck* deck);
-Deck* loadDeckFromFile(const char *filename);
-bool dealToStartTable(Deck *deck, Table *table);
-void printTable(Table *table, char lastCommand[256], char message[256]);
-void setShowAllCards(Table *table, bool isVisible);
-Deck* splitter(Deck* deck, int split);
-void printDeck(Deck* deck);
-void dealToGameTable(Table* table, Deck* deck);
-void clearTable(Table *table);
-bool moves(Table* table, char command[256]);
-bool movesCol(Table* table, char command[256]);
-void stripNewline(char *str);
-
+#include "main.h"
 
 int main() {
     srand(time(NULL));
---
+++ main.h
@@ -0,0 +1,26 @@
+//
+// Created by saleh on 30-04-2024.
+//
+
+#ifndef YUKON_MAIN_H
+#define YUKON_MAIN_H
+
+#include "deck.h"
+#include "table.h"
+
+void initializeDeck(Deck *deck);
+bool saveDeckToFile(const Deck *deck, const char *filename);
+Deck* shuffleDeck(Deck* deck);
+Deck* loadDeckFromFile(const char *filename);
+bool dealToStartTable(Deck *deck, Table *table);
+void printTable(Table *table, char lastCommand[256], char message[256]);
+void setShowAllCards(Table *table, bool isVisible);
+Deck* splitter(Deck* deck, int split);
+void printDeck(Deck* deck);
+void dealToGameTable(Table* table, Deck* deck);
+void clearTable(Table *table);
+bool moves(Table* table, char command[256]);
+bool movesCol(Table* table, char command[256]);
+void stripNewline(char *str);
+
+#endif //YUKON_MAIN_H
